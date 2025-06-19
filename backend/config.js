d336aa7e1345decac3c2d8c01fe9234badab8e4f
Joy Mathews <joymathe@gmail.com>
2025-06-19 13:20:46 +0530

    refactor: remove DynamoDB support and related files, streamline database handling

--- backend/config.js
+++ backend/config.js
@@ -8,6 +8,5 @@ module.exports = {
   DB_PORT: process.env.DB_PORT,
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
-  DB_NAME: process.env.DB_NAME,
-  DB_USED: process.env.DB_USED
+  DB_NAME: process.env.DB_NAME
 };
